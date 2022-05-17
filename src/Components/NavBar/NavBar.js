b02b0e8629c326ef62f77915d8f0299d964e8cc6
kokonnn <fran.deelizalde94@gmail.com>
2022-05-16 22:59:02 -0300

    clase navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+     <nav>
+        <h1>Azul & Rosado</h1>
+        <div className="Botones">
+            <button> Menu de la Semana </button>
+            <button> Pasteleria </button>
+            <button> Freezados </button>
+        </div>
+     </nav>   
+    )
+}
+
+export default NavBar
